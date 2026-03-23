3b0940e148be8a09ddf1be96c18c96e3972607b1
Ruben <rubenthetoad@gmail.com>
2026-03-23 12:00:16 +0100

    don’t ping

--- moth_starboard/src/starboard.rs
+++ moth_starboard/src/starboard.rs
@@ -365,7 +365,7 @@ macro_rules! starboard_message_macro {
             message = message.components(vec![components]);
 
             message = message.content(format!(
-                "{} **{} |** <#{}> <@291089948709486593>",
+                "{} **{} |** <#{}>",
                 $data.starboard_config.star_emoji,
                 $starboard_msg.star_count,
                 *$starboard_msg.channel_id
